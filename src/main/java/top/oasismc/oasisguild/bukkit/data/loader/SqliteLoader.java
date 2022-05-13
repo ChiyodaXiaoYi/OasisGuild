287e24f8baa3e39659e70f5ef21c9b3345badfb4
ChiyodaXiaoYi <linjiayi357159@gmail.com>
2022-05-14 03:06:45 +0800

    [0.1.0]Fix bug

--- src/main/java/top/oasismc/oasisguild/bukkit/data/loader/SqliteLoader.java
+++ src/main/java/top/oasismc/oasisguild/bukkit/data/loader/SqliteLoader.java
@@ -44,59 +44,57 @@ public enum SqliteLoader implements IDataLoader {
 
     @Override
     public void loadTables() {
-        Bukkit.getScheduler().runTaskAsynchronously(OasisGuild.getPlugin(), () -> {
-            Connection conn = getConnection();
-            PreparedStatement ps = null;
-            try {
-                ps = conn.prepareStatement(
-                        "CREATE TABLE IF NOT EXISTS `GuildInfo` (\n" +
-                                "`gName` varchar(20) NOT NULL,\n" +
-                                "`gIcon` varchar(20) NOT NULL,\n" +
-                                "`gLevel` int(8) NOT NULL,\n" +
-                                "`gMaxMember` int(8) NOT NULL,\n " +
-                                "`gPvp` tinyint(1) NOT NULL,\n" +
-                                "`gDesc` varchar(255) NOT NULL,\n" +
-                                " PRIMARY KEY (`gName`));");
-                ps.executeUpdate();
-                ps = getConnection().prepareStatement(
-                        "CREATE TABLE IF NOT EXISTS `GuildMembers` (\n" +
-                                "`gName`  varchar(20) NOT NULL ,\n" +
-                                "`pName`  varchar(20) NOT NULL ,\n" +
-                                "`pJob`  int(8) NOT NULL,\n" +
-                                " PRIMARY KEY (`pName`));");
-                ps.executeUpdate();
-                ps = getConnection().prepareStatement(
-                        "CREATE TABLE IF NOT EXISTS `GuildApply` (\n" +
-                                "`gName`  varchar(20) NOT NULL,\n" +
-                                "`pName`  varchar(20) NOT NULL,\n" +
-                                "`state`  tinyint(1) NOT NULL\n" +
-                                ")");
-                ps.executeUpdate();
-                ps = getConnection().prepareStatement(
-                        "CREATE TABLE IF NOT EXISTS `GuildLocation` (\n" +
-                                "`gName`  varchar(20) NOT NULL,\n" +
-                                "`gX`  int(8) NOT NULL,\n" +
-                                "`gY`  int(8) NOT NULL,\n" +
-                                "`gZ`  int(8) NOT NULL,\n" +
-                                "`gWorld`  varchar(20) NOT NULL, \n" +
-                                " PRIMARY KEY (`gName`) \n" +
-                                ");");
-                ps.executeUpdate();
-                ps = getConnection().prepareStatement(
-                        "CREATE TABLE IF NOT EXISTS `GuildChunks` (\n" +
-                                "  `gName` varchar(20) NOT NULL,\n" +
-                                "  `cX` int NOT NULL,\n" +
-                                "  `cZ` int NOT NULL,\n" +
-                                "  `cWorld` VARCHAR(50) NOT NULL" +
-                                ");"
-                );
-                ps.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                closeStatement(ps, conn);
-            }
-        });
+        Connection conn = getConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS `GuildInfo` (\n" +
+                            "`gName` varchar(20) NOT NULL,\n" +
+                            "`gIcon` varchar(20) NOT NULL,\n" +
+                            "`gLevel` int(8) NOT NULL,\n" +
+                            "`gMaxMember` int(8) NOT NULL,\n " +
+                            "`gPvp` tinyint(1) NOT NULL,\n" +
+                            "`gDesc` varchar(255) NOT NULL,\n" +
+                            " PRIMARY KEY (`gName`));");
+            ps.executeUpdate();
+            ps = getConnection().prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS `GuildMembers` (\n" +
+                            "`gName`  varchar(20) NOT NULL ,\n" +
+                            "`pName`  varchar(20) NOT NULL ,\n" +
+                            "`pJob`  int(8) NOT NULL,\n" +
+                            " PRIMARY KEY (`pName`));");
+            ps.executeUpdate();
+            ps = getConnection().prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS `GuildApply` (\n" +
+                            "`gName`  varchar(20) NOT NULL,\n" +
+                            "`pName`  varchar(20) NOT NULL,\n" +
+                            "`state`  tinyint(1) NOT NULL\n" +
+                            ")");
+            ps.executeUpdate();
+            ps = getConnection().prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS `GuildLocation` (\n" +
+                            "`gName`  varchar(20) NOT NULL,\n" +
+                            "`gX`  int(8) NOT NULL,\n" +
+                            "`gY`  int(8) NOT NULL,\n" +
+                            "`gZ`  int(8) NOT NULL,\n" +
+                            "`gWorld`  varchar(20) NOT NULL, \n" +
+                            " PRIMARY KEY (`gName`) \n" +
+                            ");");
+            ps.executeUpdate();
+            ps = getConnection().prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS `GuildChunks` (\n" +
+                            "  `gName` varchar(20) NOT NULL,\n" +
+                            "  `cX` int NOT NULL,\n" +
+                            "  `cZ` int NOT NULL,\n" +
+                            "  `cWorld` VARCHAR(50) NOT NULL" +
+                            ");"
+            );
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeStatement(ps, conn);
+        }
     }
 
     private void closeStatement(PreparedStatement statement, Connection conn) {
